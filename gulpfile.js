817005a56614dbafa77db0009235bcd40f040878
rdvdev2 <me@rdvdev2.com>
2023-02-27 18:15:09 +0100

    Build .css with sass and .user.css with nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const fs = require('fs');
+const nunjucks = require('gulp-nunjucks');
+const rename = require('gulp-rename');
 const del = require('del');
 
 function buildStyles() {
@@ -10,12 +13,22 @@ function buildStyles() {
     .pipe(gulp.dest('.'));
 }
 
+function buildUserStyles() {
+  return gulp.src('./templates/main.user.css.njk')
+    .pipe(nunjucks.compile({
+      'main_css': fs.readFileSync('./main.css').toString()
+    }))
+    .pipe(rename('main.user.css'))
+    .pipe(gulp.dest("."));
+}
+
 gulp.task('clean', function(cb) {
-  return del(['main.user.css'], cb);
+  return del(['main.css', 'main.user.css'], cb);
 })
 
 exports.buildStyles = buildStyles;
+exports.buildUserStyles = buildUserStyles;
 exports.watch = function() {
-  gulp.watch('./src/**/*.scss', buildStyles);
+  gulp.watch(['./src/**/*.scss', 'templates/main.user.css.njk'], exports.default);
 }
-exports.default = gulp.series(buildStyles);
+exports.default = gulp.series(buildStyles, buildUserStyles);
