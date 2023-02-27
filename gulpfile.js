bc21807037dfeee749f6631d65f2a55b9538b6d9
rdvdev2 <me@rdvdev2.com>
2023-02-27 18:20:54 +0100

    Refactor gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,43 @@
 'use strict';
 
 const gulp = require('gulp');
+
 const sass = require('gulp-sass')(require('sass'));
-const fs = require('fs');
 const nunjucks = require('gulp-nunjucks');
 const rename = require('gulp-rename');
+
+const fs = require('fs');
 const del = require('del');
 
+const scssSources = './src/**/*.scss';
+const scssUtilsSources = './src/utils';
+const cssOutput = './main.css'
+const templateSource = './templates/main.user.css.njk';
+const userCssOutput = './main.user.css';
+
 function buildStyles() {
-  return gulp.src('./src/**/*.scss')
-    .pipe(sass.sync({includePaths: ['src/utils'], charset: false}).on('error', sass.logError))
+  return gulp.src(scssSources)
+    .pipe(sass.sync({includePaths: [scssUtilsSources], charset: false})
+      .on('error', sass.logError))
     .pipe(gulp.dest('.'));
 }
 
 function buildUserStyles() {
-  return gulp.src('./templates/main.user.css.njk')
+  return gulp.src(templateSource)
     .pipe(nunjucks.compile({
-      'main_css': fs.readFileSync('./main.css').toString()
+      'main_css': fs.readFileSync(cssOutput).toString()
     }))
-    .pipe(rename('main.user.css'))
+    .pipe(rename(userCssOutput))
     .pipe(gulp.dest("."));
 }
 
 gulp.task('clean', function(cb) {
-  return del(['main.css', 'main.user.css'], cb);
+  return del([cssOutput, userCssOutput], cb);
 })
 
 exports.buildStyles = buildStyles;
 exports.buildUserStyles = buildUserStyles;
 exports.watch = function() {
-  gulp.watch(['./src/**/*.scss', 'templates/main.user.css.njk'], exports.default);
+  gulp.watch([scssSources, templateSource], exports.default);
 }
 exports.default = gulp.series(buildStyles, buildUserStyles);
